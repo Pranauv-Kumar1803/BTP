4a5a3c3f141be20610bf98d00f110141595776a1
Pranauv <pranauv1803@gmail.com>
2024-04-08 16:38:24 +0530

    added nuclei and nikto scanner to the tool - base version - havent done much with the report making part - run the tool as sudo

--- main.py
+++ main.py
@@ -29,13 +29,16 @@ def zapSpider(str):
     pprint(zap.core.alerts(baseurl=target))
 
 
-def main(dns: Annotated[str, typer.Option(help="This option is used for doing a DNS recon on a given domain name.\n Use this option followed by a domain name to get details of DNS records associated with that domain")] = "", zap: Annotated[str, typer.Option(help="This option is used for doing a ZAP Spider Active Scan on a given target.\n Use this option followed by a domain name to do an active scan on the domain")] = "" ):
+def main(dns: Annotated[str, typer.Option(help="This option is used for doing a DNS recon on a given domain name.\n Use this option followed by a domain name to get details of DNS records associated with that domain")] = "", zap: Annotated[str, typer.Option(help="This option is used for doing a ZAP Spider Active Scan on a given target.\n Use this option followed by a domain name to do an active scan on the domain")] = "", nikto: Annotated[str, typer.Option(help="This option is used for doing a Nikto Active Scan on a given target(Web Server).\n Use this option followed by a ip address to do an scan on the ip's web server")] = "" , nuclei: Annotated[str, typer.Option(help="This option is used for doing a Nuclei Active Scan on a given target.\n Use this option followed by a domain name to do an scan on the domain's web server")] = "" ):
     domain = dns
     z = zap
+    ni = nikto
+    nuc = nuclei
+
     if dns:
         fileName = time.time()
         os.system('echo Starting dnsrecon....... ')
-        os.system(f'dnsrecon -d {domain} -j {fileName}.json')
+        os.system(f'dnsrecon -d {domain} -j {fileName}_dns.json')
 
         with open(f'{fileName}.json', 'r') as file:
             data = file.read()
@@ -51,19 +54,23 @@ def main(dns: Annotated[str, typer.Option(help="This option is used for doing a 
 
     if z:
         print(z)
-        os.system('echo Starting ZAP Proxy ACtive Scan....... ')
+        os.system('echo Starting ZAP Proxy Active Scan....... ')
         os.system('E:')
         zapSpider(z)
+
+    if ni:
+        print(ni)
+        fileName = time.time()
+        os.system('echo Starting Nikto Scanner......')
+        os.system(f'nikto -h {ni} -o {fileName}_nikto.json -Format json')
+        os.system('echo Done with the Nikto Scanning')
+
+    if nuc:
+        fileName = time.time()
+        os.system('echo Starting Nuclei Scanner......')
+        os.system(f'nuclei -u {nuc} -json-export {fileName}_nuclei.json')
+        os.system('echo Done with the Nuclei Scanning')
     
 if __name__ == "__main__":
-    # typer.run(main)
-    with open(f'1712313377.49659.json', 'r') as file:
-            data = file.read()
-            json_data = json.loads(data)
-
-    for j in json_data:
-        if j['type'] != 'ScanInfo' and j['type']!='Text':
-            ip = [j['address']]
-            print(ip)
-            os.system(f'nikto -h {ip[0]}')
+    typer.run(main)
     
