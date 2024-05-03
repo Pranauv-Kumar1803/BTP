09bcda120a16aaa6cc26949a33d534c83044017f
Pranauv <pranauv1803@gmail.com>
2024-05-03 12:59:30 +0530

    added dirsearch to main.py, changed routes for files and added dirsearch report to the Reports folder and merged zip file into proper folders

--- Scripts/using_Zap.py
+++ Scripts/using_Zap.py
@@ -16,32 +16,25 @@ def start_zap_daemon(zap_path):
 
 
 def zap_scan(target, api_key, result_file):
-    # Initialize ZAP instance
     zap = ZAPv2(apikey=api_key, proxies={
                 'http': 'http://127.0.0.1:1025', 'https': 'http://127.0.0.1:1025'})
 
-    # Load the policy for OWASP Top 10
     zap.ascan.set_option_attack_policy('policyName', 'OWASP Top 10')
 
-    # Spider the target
     print('Spidering target {}'.format(target))
     zap.spider.scan(target)
     while int(zap.spider.status()) < 100:
-        # Loop until the spider has finished
         print('Spider progress %: {}'.format(zap.spider.status()))
         time.sleep(5)
     print('Spider completed')
 
-    # Active Scan the target
     print('Active Scanning target {} for OWASP Top 10 vulnerabilities'.format(target))
     zap.ascan.scan(target)
     while int(zap.ascan.status()) < 100:
-        # Loop until the scanner has finished
         print('Active Scan progress %: {}'.format(zap.ascan.status()))
         time.sleep(5)
     print('Active Scan completed')
 
-    # Collect alerts
     alerts = zap.core.alerts(baseurl=target)
 
     # Write alerts to file
@@ -61,11 +54,10 @@ def zap_scan(target, api_key, result_file):
 
 
 if __name__ == "__main__":
-    # Update this with the correct path to your ZAP installation
-    zap_path = '"C:/Program Files/ZAP/Zed Attack Proxy"'
+    zap_path = '../Reports'
     target = 'https://www.youtube.com/'
     api_key = 'jhruomht0d438c8hej49nqphtt'
-    result_file = 'scan_results.txt'
+    result_file = 'zap_scan_results.txt'
 
     start_zap_daemon(zap_path)
 
